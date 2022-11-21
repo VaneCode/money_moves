1a01b435362c311d86fcf83f6b03715996e874cc
vanessaBonito <vanebonito@hotmail.com>
2022-11-21 15:34:50 -0500

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'splashes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  root "splashes#index"
 end
