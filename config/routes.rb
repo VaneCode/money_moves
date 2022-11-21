0a55db6e53c5e35580bf8b372c8ccbabfc9f291e
vanessaBonito <vanebonito@hotmail.com>
2022-11-21 16:18:30 -0500

    Create model for group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :groups
   resources :users
   get 'splashes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
