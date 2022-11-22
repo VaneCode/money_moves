010d4753572f63c11cca72db21f6dd2e04081414
vanessaBonito <vanebonito@hotmail.com>
2022-11-22 12:09:20 -0500

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = %i[name email password password_confirmation current_password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@
 
 Rails.application.routes.draw do
   devise_for :users # Routes for authentication
+  # Defines the root path route ("/")
+  root "splashes#index"
   resources :spends
   resources :groups
   resources :users
-  get 'splashes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "splashes#index"
+  
 end
