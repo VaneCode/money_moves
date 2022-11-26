bedaedd65ff0dd6d40157e6dd688cc63d32aaf5b
vanessaBonito <vanebonito@hotmail.com>
2022-11-26 12:50:21 -0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   # Associations
   has_many :spends, foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
