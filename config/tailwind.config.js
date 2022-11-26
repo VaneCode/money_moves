dda332fdd733136ff5a57206903e66fb4676cfa7
vanessaBonito <vanebonito@hotmail.com>
2022-11-26 00:26:17 -0500

    Remove flowbite

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -26,6 +25,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('flowbite/plugin'),
   ]
 }
