e63f6d988dfb13853291a5716b878675fbeb145d
vanessaBonito <vanebonito@hotmail.com>
2022-11-26 09:52:55 -0500

    Update config env configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,11 +52,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'money-moves.onrender.com' }
+  # config.action_mailer.default_url_options = { host: 'money-moves.onrender.com' }
 end
